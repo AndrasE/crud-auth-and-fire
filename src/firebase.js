c206719728745f477bf9dd6fa370aa6621d1ddd0
Andras E <rohadtsajt@gmail.com>
2022-04-22 21:07:45 +0100

    add env to config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDakBuesprwJu4AguOOXCjZ4TJoFHQ268A",
-  authDomain: "crud-auth-and-fire.firebaseapp.com",
+  apiKey: "REACT_APP_API_KEY",
+  authDomain: "REACT_APP_AUTH_DOMAIN",
   projectId: "crud-auth-and-fire",
-  storageBucket: "crud-auth-and-fire.appspot.com",
-  messagingSenderId: "979843203476",
-  appId: "1:979843203476:web:137eee01897e61bfab4f81",
-  measurementId: "G-PC5DQZL55T"
+  storageBucket: "REACT_APP_STORAGE_BUCKET",
+  messagingSenderId: "REACT_APP_MESSAGING_SENDER_ID",
+  appId: "REACT_APP_APP_ID",
+  measurementId: "REACT_APP_MEASUREMENTID_ID"
 };
   
 
@@ -19,12 +19,3 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
 
-// const firebaseConfig = {
-//   apiKey: "REACT_APP_API_KEY",
-//   authDomain: "REACT_APP_AUTH_DOMAIN",
-//   projectId: "project-mygag",
-//   storageBucket: "REACT_APP_STORAGE_BUCKET",
-//   messagingSenderId: "REACT_APP_MESSAGING_SENDER_ID",
-//   appId: "REACT_APP_APP_ID",
-//   measurementId: "REACT_APP_MEASUREMENTID_ID"
-// };
