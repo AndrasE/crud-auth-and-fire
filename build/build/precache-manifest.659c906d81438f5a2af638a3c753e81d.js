67c958dc3443446eee4ac5fac006631bdee35189
Andras E <rohadtsajt@gmail.com>
2022-05-26 21:08:22 +0100

    fddfgdf
    dassdds

---
+++ build/build/precache-manifest.659c906d81438f5a2af638a3c753e81d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "247a82d4ecfc5aa1df73",
+    "url": "/static/js/main.5a2b554b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d8ae645ec4d499e3d02",
+    "url": "/static/css/2.15dde489.chunk.css"
+  },
+  {
+    "revision": "8d8ae645ec4d499e3d02",
+    "url": "/static/js/2.2016962d.chunk.js"
+  },
+  {
+    "revision": "ce59f360c16c5605883c286d23528418",
+    "url": "/index.html"
+  }
+];
