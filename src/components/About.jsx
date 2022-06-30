b0f13e9d8bbe02d84be1650c991449c06c290c5b
Andras E <rohadtsajt@gmail.com>
2022-06-30 22:37:35 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,7 @@ const About = () => {
     return (
         <div className="about-div">
 
-<<<<<<< HEAD
             <h4>Hi there<span role="img" aria-label="wave">👋</span></h4>
-=======
-            <h4>Hi there  </h4>
->>>>>>> 7af5806a49ac835dc419d5eb76b7c074f657f8d9
 
             <h6>Welcome to the "about" section!</h6>
             <br />
@@ -22,11 +18,7 @@ const About = () => {
                 And does this with your control (and algorithm!). Facebook, Instagram, Netflix, Uber, Reddit they all use React.
                 Just imagine if they would work staticly. Every like, every click would have to reload the whole of the webpage for you (the code also need to change in the backend by something/someone). That would be very
                 annoying! Not to mention all data would get sent over back and forth for you to consume. Not a good idea. This is React! When you press a like button
-<<<<<<< HEAD
                 basically just one thing going to be rerendered on the webpage. The little heart fills in red, the thumb goes up, the taxi is coming your way on the map. Beautiful isn't it?<span role="img" aria-label="smiley">😊</span> If you would like hear more about static/dynamic webpages <a href="https://www.youtube.com/watch?v=_wFJj94kSTU">click here</a>.</p>
-=======
-                basically just one thing going to be rerendered on the webpage. The little heart fills in red, the thumb goes up, the taxi is coming your way on the map. Beautiful isn't it? If you would like hear more about static/dynamic webpages <a href="https://www.youtube.com/watch?v=_wFJj94kSTU">click here</a>.</p>
->>>>>>> 7af5806a49ac835dc419d5eb76b7c074f657f8d9
             <p>If you would like read more about how this page was created, what hurdles I faced with <a href="https://www.youtube.com/watch?v=NhR88ZLCuFs&t=22s">click here</a>.</p>
 
         </div>
