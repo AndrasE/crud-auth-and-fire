998d1d09706166af08b2fa1c6692c9beb108d274
Andras E <rohadtsajt@gmail.com>
2022-07-05 17:23:27 +0100

    about update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ae45898fb5f2599e9535550333baff5.js"
+  "/precache-manifest.8a7493b03d3c1623eee23cba30c8704b.js"
 );
 
 workbox.clientsClaim();
